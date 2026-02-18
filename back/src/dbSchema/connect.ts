3f9e89fd99e8b20da31807a6c3c8e2d6191ff656
Chaitanya1130 <svedula4@gitam.in>
2026-02-18 23:58:10 +0530

    changed connect

--- back/src/dbSchema/connect.ts
+++ back/src/dbSchema/connect.ts
@@ -2,7 +2,7 @@
 import pg from 'pg';
 const { Pool } = pg;
 
-const pool = new Pool({
+export const pool = new Pool({
   connectionString: process.env.DATABASE_URL, // It MUST look for this key
   ssl: {
     rejectUnauthorized: false // This is required for Neon
