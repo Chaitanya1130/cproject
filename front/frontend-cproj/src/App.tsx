5bc1dc6d1c161567db0add7ac96fff5c3ad09b65
Chaitanya1130 <svedula4@gitam.in>
2026-02-19 11:32:12 +0530

    changed App.tsx

--- front/frontend-cproj/src/App.tsx
+++ front/frontend-cproj/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+
 import './Css/App.css'
 import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
 import SignUp from "./MainComponents/Cards/Auth/SignUp";
